827f35bc4a629657899e9615051f4c842e737d7b
Vincent Yono <vincent.yono@binus.ac.id>
2022-03-05 13:16:55 +0700

    Question 8

---
+++ Drill1/Question8.java
@@ -0,0 +1,16 @@
+package Drill1;
+
+class Question8 {
+    public static void main(String[] args)
+    {
+        System.out.println("  *********     ***       *        *      ");
+        System.out.println("  *       *   *     *    ***      * *     ");
+        System.out.println("  *       *  *       *  *****    *   *    ");
+        System.out.println("  *       *  *       *    *     *     *   ");
+        System.out.println("  *       *  *       *    *    *       *  ");
+        System.out.println("  *       *  *       *    *     *     *   ");
+        System.out.println("  *       *  *       *    *      *   *    ");
+        System.out.println("  *       *   *     *     *       * *     ");
+        System.out.println("  *********     ***       *        *      ");
+    }
+}
